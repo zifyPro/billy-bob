2ef3b291addfa842f501eed1ecc6c39d9d84de92
Diaz027 <dami27cito@gmail.com>
2024-03-01 16:16:53 -0300

    aaa

--- billy-bob/src/Redux/store.js
+++ billy-bob/src/Redux/store.js
@@ -1,11 +1,16 @@
-import { createStore, applyMiddleware, compose } from "redux";
-import thunkMiddleware from "redux-thunk";
-import reducer from "./reducer.js";
+// import { createStore, applyMiddleware, compose } from "redux";
+// import thunkMiddleware from "redux-thunk";
+// import reducer from "./reducer.js";
 
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // esta línea es para conectar con la extensión del navegador => REDUX DEVTOOLS
+// const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // esta línea es para conectar con la extensión del navegador => REDUX DEVTOOLS
 
-const store = createStore(
-  reducer,
-  composeEnhancer(applyMiddleware(thunkMiddleware)) // esta línea es para poder hacer peticiones a un server
-);
-export default store;
+// const store = createStore(
+//   reducer,
+//   composeEnhancer(applyMiddleware(thunkMiddleware)) // esta línea es para poder hacer peticiones a un server
+// );
+// export default store;
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import reducer from "./reducer";
+
+export default createStore(reducer, applyMiddleware(thunk));
--- billy-bob/src/main.jsx
+++ billy-bob/src/main.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import store from "./Redux/store";
+import Store from "./Redux/store";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
+  <Provider store={Store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
