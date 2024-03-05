a46ffdeeede7e60d977a572f8cb87634c962f7f7
Gabriel J. <106175930+zuoki@users.noreply.github.com>
2024-03-05 11:33:43 -0300

    nav bar 2.0

--- billy-bob/src/App.jsx
+++ billy-bob/src/App.jsx
@@ -3,13 +3,12 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./views/home/home";
 import Store from "./views/store/store";
 import Menus from "./components/menu/menu";
-// import Navbar from "./components/navBar/NavBar";
+import Navbar from "./components/navBar/NavBar";
 
 function App() {
   return (
     <div className="bg-[url('https://res.cloudinary.com/divxrmzge/image/upload/v1708176856/ojmk9i0nuuov2f2ahtke.jpg')] bg-cover bg-center">
-      {/* <Navbar /> */}
-      <Menus />
+       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/store" element={<Store />} />
--- billy-bob/src/components/navBar/NavBar.jsx
+++ billy-bob/src/components/navBar/NavBar.jsx
@@ -1,50 +1,17 @@
 import { FaUser } from "react-icons/fa";
 import { Link } from "react-router-dom";
-import { useOrganizationList } from "@clerk/nextjs";
-import { UserButton, useUser } from "@clerk/nextjs";
 import Menus from "../menu/menu";
 
 const Navbar = () => {
-  const { organizationList, isLoaded } = useOrganizationList();
-  let isAdmin = false;
+  
 
-  if (isLoaded) {
-    const adminOrganization = organizationList.find(
-      (org) => org.membership.role === "org:admin"
-    );
-    if (
-      adminOrganization &&
-      adminOrganization.membership.role === "org:admin"
-    ) {
-      isAdmin = true;
-    }
-  }
 
-  const user = useUser();
-  const isLogin = user?.isSignedIn;
 
   return (
     <>
       <div className="flex flex- justify-around items-center pt-6 bg-custom-gray rounded-md mx-auto mt-3  w-11/12 h-12 md:w-2/4">
         <div className="flex items-center -ml-10 -mt-6">
-          {isLogin ? (
-            <>
-              <UserButton
-                className="rounded-full items-center  "
-                afterSignOutUrl="/"
-              />
-              <h4 className="font-sans font-semibold italic text-xs text-nowrap my-auto mx-2 ">
-                {"Hola " + user.user.username + "!"}
-              </h4>
-            </>
-          ) : (
-            <a
-              href="/sign-in"
-              className="   text-white rounded-sm my-1.5 mx-1.5 place-content-center text-sm h-6.5 text-center "
-            >
-              <FaUser />
-            </a>
-          )}
+         
         </div>
 
         <div className="-mt-6 -mr-10">
@@ -54,15 +21,7 @@ const Navbar = () => {
       <div className="flex justify-center items-center ">
         <img className="w-3/6 md:w-1/6 " src="/logo.png" alt="logo billiebob" />
       </div>
-      {isAdmin && (
-        <div className="flex flex- justify-around items-center pt-1">
-          <Link className=" p-2 bg-custom-gray rounded-md" href="/panelAdmin">
-            <button className=" text-yellow-500 font-semibold">
-              panel administrador
-            </button>
-          </Link>
-        </div>
-      )}
+
     </>
   );
 };
